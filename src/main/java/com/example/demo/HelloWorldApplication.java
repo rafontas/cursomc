e1c2bed9c5634e4c53b7ec92618b973ba7804e87
Rafael Freitas <rafontas@gmail.com>
2018-06-13 20:54:20 -0300

    Criando instanciação do banco.

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -1,12 +1,31 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HelloWorldApplication {
+import com.example.demo.domain.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class HelloWorldApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository Repositorio;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Categoria 2");
+		Categoria c2 = new Categoria(null, "Categoria 3");
+		
+
+		Repositorio.saveAll(Arrays.asList(c1, c2));
 	}
 }
